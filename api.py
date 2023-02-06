4e4d80b8597a296b33200f82a9e2735c76e198a1
perye <lipengyu.cn@hotmail.com>
2023-02-06 10:25:50 +0800

    v1

---
+++ api.py
@@ -0,0 +1,27 @@
+# venv/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2/2/2023 10:55 pm
+# @Author  : Perye (Pengyu LI)
+# @File    : api.py
+# @Software: PyCharm
+
+from flask import request
+from flask_parameter_validation import ValidateParameters, Route, Json, Query
+
+from hang_seng import app
+from error_handler import handle_param_validation
+import util.currencies
+import scraper
+
+
+@app.route('/rate')
+@ValidateParameters(handle_param_validation)
+def get_rate(
+        base_currency: str = Query('HKD', func=util.currencies.validate_currency),
+        currency: str = Query('HKD', func=util.currencies.validate_currency)
+):
+    if 'GET' == request.method:
+        scraper_func = getattr(scraper, 'scrape_exchange_rate_' + base_currency.lower())
+        return scraper_func().get(currency) or {}
+    elif 'DELETE' == request.method:
+        scraper.clear_cache(base_currency)
---
+++ scraper.py
@@ -0,0 +1,45 @@
+# venv/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 2/2/2023 10:03 pm
+# @Author  : Perye (Pengyu LI)
+# @File    : scraper.py
+# @Software: PyCharm
+
+from typing import Dict
+import requests
+import json
+from dateutil import parser
+
+import util.currencies
+from extensions import cache
+
+
+@cache.memoize(timeout=300)
+def scrape_exchange_rate_hkd() -> Dict:
+    tt_data = json.loads(requests.get(
+        'https://rbwm-api.hsbc.com.hk/pws-hk-hase-rates-papi-prod-proxy/v1/fxtt-exchange-rates'
+    ).text)
+
+    last_updated = parser.parse(tt_data['lastUpdateTime']).isoformat()
+    tt_data = tt_data['fxttExchangeRates']
+
+    note_data = json.loads(requests.get(
+        'https://rbwm-api.hsbc.com.hk/pws-hk-hase-rates-papi-prod-proxy/v1/fxnotes-exchange-rates'
+    ).text)['fxnoteExchangeRates']
+
+    note_result = {item['ccyDisplayCode']: {
+        'banknotes_bank_buy': float(item['noteBuyRate']) if item['noteBuyRate'] else None,
+        'banknotes_bank_sell': float(item['noteSellRate']) if item['noteSellRate'] else None,
+    } for item in note_data}
+
+    return {item['ccyDisplayCode']: {
+        'telegraphic_transfer_bank_buy': float(item['ttBuyRate']) if item['ttBuyRate'] else None,
+        'telegraphic_transfer_bank_sell': float(item['ttSellRate'])if item['ttSellRate'] else None,
+        'last_updated': last_updated,
+        **(note_result.get(item['ccyDisplayCode']) or {'banknotes_bank_buy': None, 'banknotes_bank_sell': None})
+    } for item in tt_data}
+
+
+def clear_cache(base_currency: str):
+    assert base_currency.upper() in util.currencies.allowed_currencies
+    cache.delete_memoized(eval('scrape_exchange_rate_' + base_currency.lower()))
